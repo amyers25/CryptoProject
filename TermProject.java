802ec8c186ac3c57d7fc1784aa30b339d20d8537
amyers25 <amyers25@students.towson.edu>
2016-11-30 17:36:22 -0500

    Add files via upload
    Initial Upload, Blank main and no methods added.

---
+++ TermProject.java
@@ -0,0 +1,15 @@
+
+package termproject;
+
+/*
+Alex Myers
+Evan Myers
+Justin Jones
+ */
+public class TermProject {
+
+    public static void main(String[] args) {
+        System.out.println("eat my shorts");
+    }
+    
+}
